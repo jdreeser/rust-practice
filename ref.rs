7a11d2020dd23c9c38ed484f59ec0a46402cb4e6
Jack Reeser <jdreeser@gmail.com>
2017-06-09 12:45:42 -0600

    added tuples

--- ref.rs
+++ ref.rs
@@ -1,27 +1,31 @@
 // reference for getting used to rust
-use std::fmt; // use a library
+use std::fmt; 												   // use a library
 
-#[derive(Debug)]  // derive a trait implementation
-struct Name(i32); // define a struct
+#[derive(Debug)]                               // derive a trait implementation
+struct Name(i32);                                            // define a struct
 
-impl fmt::Display for Name { // manually implement a trait
+impl fmt::Display for Name {                      // manually implement a trait
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { // define a function
-		write!(f, "{}", self.0) // call a macro, return value
+		write!(f, "{}", self.0)                   // call a macro, return value
 	}
 }
 
 #[derive(Debug)]
-struct Named { // define a struct with named member variables
-	x: i32,    // define signed integer
-	list: Vec<i32>, // define vector
+struct Named {  			     // define a struct with named member variables
+	x: i32,        									   // define signed integer
+	list: Vec<i32>, 										   // define vector
 }
 
-fn main() { // define main function
-	let name = Name(3); // declare struct variable
+fn main() {             								// define main function
+	let name = Name(3); 							 // declare struct variable
 	let named = Named { x: 3, list: vec![1, 2, 3] };
 	println!("{:?}", name);
 	println!("{:?}", named);
 
-	let mil = 1_000_000u32; // suffix type annotation
-	let mil: u32 = 1_000_000; // type annotation
+	let mil = 1_000_000u32;   						  // suffix type annotation
+	let mil: u32 = 1_000_000; 								 // type annotation
+
+	let tup = (33i32, true);  								  // defining tuple
+	let tup = (32u8,)         							   // one element tuple
+	let (num, boolean) = tup; 		  // binding tuple member elements to names
 }
